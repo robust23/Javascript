30d9dcbd7b760a8eb35f52cae60080cd0263c415
theregularguy <97795612+robust23@users.noreply.github.com>
2023-10-23 07:23:15 +0000

    publish

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 14453
+let accountEmail = "qwerty@google.com"
+var accountPassword = "12345"
+accountcity = "Delhi"
+console.log(accountId)
+
+accountEmail = "xppen@google.com"
+accountPassword = "qwerty"
+accountcity = "Haryana"
+/* variable can be changed without const & let */
+
+//prefer to use const and let because var has scope issue in javascript {}//
+
+console.table([accountId,accountEmail,accountPassword,accountcity])
+
---
+++ 01_basics/02_dataypes.js
@@ -0,0 +1,8 @@
+"use strict"; //treat all js code as newer version 
+//alert (3+3) // this will show error as we are using node js not browser
+
+console.log(3 + 3) //arithmatic operation
+console.log("Chitwan")
+
+console.log(typeof undefined);
+console.log(typeof null)
