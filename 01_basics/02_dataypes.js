cbd0265914000b5ed15f8c8f97b2963cd8f2be2f
theregularguy <97795612+robust23@users.noreply.github.com>
2023-10-23 07:24:34 +0000

    push

--- 01_basics/02_dataypes.js
+++ 01_basics/02_dataypes.js
@@ -1,7 +1,7 @@
 "use strict"; //treat all js code as newer version 
 //alert (3+3) // this will show error as we are using node js not browser
 
-console.log(3 + 3) //arithmatic operation
+console.log(3 + 3) //arithmatic operation in js
 console.log("Chitwan")
 
 console.log(typeof undefined);
