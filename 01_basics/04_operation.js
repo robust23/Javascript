48c0dfdbbee1d811b265136e43a116a5387b0574
theregularguy <97795612+robust23@users.noreply.github.com>
2023-11-02 08:01:08 +0000

    data types

---
+++ 01_basics/04_operation.js
@@ -0,0 +1,13 @@
+let score =  100
+score ++ ;
+console.log(score);
+// there is difference between prefix and post fix 
+//let x = 3;
+//const y = x++;
+// x is 4; y is 3
+//this is post fix example 
+
+//let x = 3;
+//const y = ++x;
+// x is 4; y is 4
+//this is prefix example
---
+++ 01_basics/05_comparisondatatypes.js
@@ -0,0 +1,22 @@
+console.log(2>2)
+console.log(2<2)
+console.log(2==2)
+console.log(2>=2)
+console.log(2<=2)
+console.log(2!=2)
+// above are the basic examples of data comparison
+// problem arrises when we compare two different data types 
+
+console.log("2" > 1)
+console.log("02" < 3)
+console.log(null>0)
+console.log(null == 0)
+console.log(null >= 0)
+//in the null case js convert null to 0 automatically 
+
+console.log(undefined == 0 )
+console.log(undefined > 0 )
+console.log(undefined < 0 )
+// === it is strict check it will also check the datatype 
+
+console.log("2" === 2)
